db3f4f5910ef90430e4b6b55165b0a38753240b2
Maria <maria.meneses@eia.edu.co>
2025-02-25 01:29:24 -0500

    prod

--- __tests__/test-utils.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import { SessionProvider } from "next-auth/react";
-
-export function MockSessionProvider({ children }: { children: ReactNode }) {
-  return <SessionProvider session={null}>{children}</SessionProvider>;
-}
