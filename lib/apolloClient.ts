1af1da71b023f038dd0a38c7b6263e65747cc80e
Maria <maria.meneses@eia.edu.co>
2025-02-24 23:06:17 -0500

    create uri to connect to backend

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -2,10 +2,8 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 
 const client = new ApolloClient({
-
-    //uri: 'http://localhost:3000/api/graphql',
-    uri:'https://prueba-tecnica-fullstack-git-main-mariamm1110s-projects.vercel.app/api/graphql',
-    cache: new InMemoryCache()
+    uri: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/graphql",
+    cache: new InMemoryCache(),
 });
 
 export default client;
